a536e0bca60d65b235f801a5b51f7b0847ac235b
annechege <annechege780@gmail.com>
2021-05-18 14:44:51 +0300

    create and delete event

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { QUOTE } from '../mock-quote';
 import { Quote } from '../quote';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -12,10 +12,15 @@ import {QuoteService} from '../services/quote.service';
 export class CardComponent implements OnInit {
   quotes: Quote[] = [];
   faTimes = faTimes;
+  @Output() onDeleteQuote: EventEmitter<Quote> = new EventEmitter
 
   constructor(private quoteService: QuoteService) { }
 
   ngOnInit(): void {
     this.quoteService.getQuotes().subscribe((quotes) => (this.quotes = quotes));
+  }
+
+  onDelete(quote: any){
+    this.onDeleteQuote.emit(quote)
   }
 }
