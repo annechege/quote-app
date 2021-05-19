71bd29753364f9fb93c6fab19259bae9f803d61b
annechege <annechege780@gmail.com>
2021-05-19 12:25:45 +0300

    adding qoutes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote (1,'Ann','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2021,4,15),0,0),
-    new Quote (1,'Ann','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2021,4,15),0,0),
+    new Quote (1,'Anne','The purpose of our lives is to be happy.','Dalai Lama.', new Date(2021,4,15),0,0),
+    new Quote (2,'Jane','Life is what happens when you are busy making other plans.','John Lennon.', new Date(2021,4,12),0,0),
+    new Quote (3,'Jacky','Get busy living or get busy dying.' ,'Stephen King.', new Date(2021,4,10),0,0),
+    new Quote (4,'Amna','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2021,4,11),0,0),
 
   ];
   get sortQuotes() {
